bee5f3a3d3bfb5fa81da4f654618ccfc002962ed
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-07-19 12:16:56 -0300

    mend

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ var app = express();
 
 app.use(cors({origin:true, credentials: true}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
