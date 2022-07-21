68551b2d63d0017f0abba7dd6bde8c681e9a7089
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-07-21 15:24:11 -0300

    mend

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ require("dotenv").config();
 
 var app = express();
 
-app.use(cors({origin:true, credentials: true}))
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -25,6 +23,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+
+app.use(cors({origin:true, credentials: true}))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended:true}));
 
