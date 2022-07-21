c2e4582d489eacb2f9e00f7a2fb3ca0cc3af7e2a
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-07-21 15:44:49 -0300

    mend

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-//app.use(cors({origin:true, credentials: true}))
-app.use(cors({origin: '*'}));
+app.use(cors({origin:true, credentials: true}))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended:true}));
 
