7f28988d1dd6c99fd2b21989af1a32cfe92b6773
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-07-21 11:28:52 -0300

    reworking images backend

--- app/controllers/image.controller.js
+++ app/controllers/image.controller.js
@@ -27,18 +27,22 @@ var storage=multer.diskStorage({
 var upload=multer({storage:storage});
 
 exports.upload=(req, res, done)=>{
+    console.log(req.body)
     const newImage=new Image({
         imageUrl: req.body.imageUrl,
         imageDesc: req.body.imageDesc.trim(),
     })
+    
     Image.update({'imageDesc':req.body.imageDesc},{$set:{imageUrl:newImage.imageUrl, imageDesc:newImage.imageDesc}},{upsert:true});
 }
 
 exports.showImg=(req,res, done)=>{
-    Image.findOne({imageDesc: req.params.imageDesc}).then((image)=>{
+    Image.findOne({imageDesc: req.params.param}).then((image)=>{
         if(!image){
             return res.status(401).json({succsses:false, msg:'Could not find image'})
         }
-        return res.status(200).json(image);
+        else{
+            return res.status(200).json(image);
+        }
     })
 }
--- app/routes/images.routes.js
+++ app/routes/images.routes.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var router = express.Router();
+
+var router = require("express").Router();
 const imgCtrl = require('../controllers/image.controller');
 
 router.post("/upload", imgCtrl.upload)
-router.get("/getImages", imgCtrl.showImg)
+router.get("/getImages/:param", imgCtrl.showImg)
 
 
 module.exports = router;
