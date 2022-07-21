eafe30cde06941177dd442ceee6543542dbfadf2
Ian Petraccaro Cantero <ianpetraccaro@gmail.com>
2022-07-21 10:42:30 -0300

    Modified upload limit for images

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended:true}));
 
 app.use(express.json());
 
