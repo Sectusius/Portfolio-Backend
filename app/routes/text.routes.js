dbc72303cec9c06ec62886d673e773943cfcf618
Sectusius <ianpetraccaro@gmail.com>
2022-07-25 08:21:02 -0300

    modified getText route

--- app/routes/text.routes.js
+++ app/routes/text.routes.js
@@ -3,6 +3,6 @@ const textCtrl = require('../controllers/text.controller')
 
 router.post("/uploadText",textCtrl.uploadText)
 
-router.get("/text/:text_location",textCtrl.showText)
+router.get("/getText/:text_location",textCtrl.showText)
 
 module.exports=router
