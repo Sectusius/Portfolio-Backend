b89f1abc598ca75aef484a236172bda81675c30d
Sectusius <ianpetraccaro@gmail.com>
2022-07-27 08:57:56 -0300

    modified user routes

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,7 +4,6 @@ const userCtrl = require('../controllers/user.controller')
 router.post("/signup",userCtrl.signUp)
 router.post("/login",userCtrl.logIn)
 
-router.get("/user",userCtrl.getUsers)
 router.get("/user/:user_id",userCtrl.showUser)
 
 module.exports=router
